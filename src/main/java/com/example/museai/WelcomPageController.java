0bcb221ebbd6e2dadfb86f7471c7fee6ff4749bc
SierraHEd <sierraedwards123@yahoo.com>
2024-02-25 12:06:12 -0500

    Changed Hello to WelcomePage

--- src/main/java/com/example/museai/WelcomPageController.java
+++ src/main/java/com/example/museai/WelcomPageController.java
@@ -3,7 +3,7 @@ package com.example.museai;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class WelcomPageController {
     @FXML
     private Label welcomeText;
 
